c10137a5b48a2fd202b00a38db1f1fc4a9ca4d24
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-07-29 00:55:51 +0400

    made a simple async echo server

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,4 +1,27 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello World!");
+async fn main() -> io::Result<()> {
+    let addr = "127.0.0.1:6969";
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    tokio::spawn(async move {
+        loop {
+            let (stream, _) = listener.accept().await.unwrap();
+            tokio::spawn(async move {
+                let mut buf = [0; 10];
+                let (mut r_stream, mut w_stream) = stream.into_split();
+                let Ok(n) = r_stream.read(&mut buf[..]).await else {
+                    return;
+                };
+                let _ = w_stream.write_all(&buf[..n]).await;
+            });
+        }
+    });
+
+    let mut stream = tokio::net::TcpStream::connect(addr).await?;
+    stream.write_all(b"ping").await?;
+    let mut buf = [0; 10];
+    let n = stream.read(&mut buf[..]).await?;
+    println!("{}", String::from_utf8_lossy(&buf[..n]));
+    Ok(())
 }
