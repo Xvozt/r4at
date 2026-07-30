462cc478b4a679100558a1482828ee9a60f44bcb
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-07-31 00:16:14 +0400

    bounded channel explore

--- examples/mpsc_play.rs
+++ examples/mpsc_play.rs
@@ -1,4 +1,22 @@
+use std::time::Duration;
+
 #[tokio::main]
 async fn main() {
-    println!("Hellow world!");
+    let (tx, mut rx) = tokio::sync::mpsc::channel(10);
+    tokio::spawn(async move {
+        for i in 0..15 {
+            println!("Trying to send: {i}");
+
+            if let Err(e) = tx.send(i).await {
+                println!("Reciever dropped: {e}");
+                return;
+            }
+            println!("Successfully sent: {i}");
+        }
+    });
+
+    while let Some(i) = rx.recv().await {
+        tokio::time::sleep(Duration::from_millis(500)).await;
+        println!("Recieved: {i}");
+    }
 }
